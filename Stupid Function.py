472270c8fef1e684931bca5446e66a5abfb23d0d
Sarah <Sarah>
2026-06-28 16:39:07 -0400

    Facts

--- Stupid Function.py
+++ Stupid Function.py
@@ -1,3 +1,18 @@
 def Shape (x,y):
-    print (x*y)
+    return (x*y)
 Shape (455555550,5)
+print (Shape(20, 20))
+width =  int(input("What is the width"))
+length = int(input("What is the length"))
+print (Shape(width,length))
+
+
+def fc (x):
+    return ((x-32)*(5/9))
+far = float(input("What is the fehrenheit?"))
+print ("The celsius is "+ str(fc(far)))
+
+def cf (x):
+    return (x*1.8 +32)
+cell = float(input("What is the celcius?"))
+print ("The Fahrenheit is"+str(cf(cell)))
