a15526f52a5cf0c667b14048d4561c6305bc3b73
Sarah <Sarah>
2026-06-29 13:29:54 -0400

    yota tingy

--- Sorta Sorty.py
+++ Sorta Sorty.py
@@ -1,1 +1,12 @@
-dalist = []
+def swop (l,f,s):
+    t = l[f]
+    l[f] =l[s]
+    l[s]=t
+def sorty (l):
+    for i in range(0,len(l)):
+        for j in range (0,len(l)-1):
+            if l[j]> l[j+1]:
+             swop(DeYist,j,j+1)
+DeYist = [1,3,4,5,2,4,56,1,43,6,2,5,2,4,-54]
+sorty(DeYist)
+print (DeYist)
---
+++ Sorta Sorty2.py
@@ -0,0 +1,4 @@
+def medge(l,f,s):
+    y=f
+    r=(f+s)//2
+    while 
